048707d347e009c027b271174735a1f046cf7aa5
ESBass <149180647+ESBass@users.noreply.github.com>
2024-12-05 16:17:43 +0000

    Did day 2 part 1

--- Day 2/main.c
+++ Day 2/main.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define UP 1
+#define DOWN -1
+
+int GetSign(int i){
+    if(i < 0) return -1;
+    if(i > 0) return 1;
+    else return 0;
+}
+
 int main(int argc, char** argv){
 
-    int safeReports = 0, unsafeReports = 0;
+    int safeReports = 0;
     FILE* reportstext;
 
     reportstext = fopen("../Day 2/input.txt", "r");
@@ -13,26 +22,57 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    char str[50] = {0};
+    char line[50] = {0};
+    const char delim[2] = " ";
 
-    int dir = 0;
-    int current = 0;
-    int prev = 0;
+    int it = 1;
 
-    while(fgets(str, 50, reportstext)){
-        printf("%s", str);
+    while(fgets(line, 50, reportstext)){
+        int numreps = 1;
+        int reports[10] = {0};
 
-        char* tok = strtok(str, ' ');
+        if(line[0] == '\n') continue;
 
-        current = tok - '0';
-
-        while(tok != NULL){
-            prev = current;
-            printf("%s", tok);
-            tok = strtok(NULL, ' ');
-        }
+        printf("%s", line);
         
+
+        char* token = strtok(line, delim);
+        // printf("%s\n", token);
+
+        while(token != NULL){
+            //printf("%s\n", token);
+            reports[numreps - 1] = atoi(token);
+            token = strtok(NULL, delim);
+            numreps++;
+        }
+
+        printf("\n");
+
+        int dir = GetSign(reports[1] - reports[0]);
+        int prev = 0;
+
+        if(!dir) goto next;
+
+        for(int i = 0; i < numreps - 2; i++){
+            int diff = reports[i+1] - reports[i];
+
+            if(abs(diff) < 1 || abs(diff) > 3){
+                goto next;
+            }
+
+            if(GetSign(diff) != dir) goto next;
+        }
+
+        safeReports++;
+
+        printf("Report %d is safe, there are %d safe reports\n", it, safeReports);
+
+next:
+
+        it++;
     }
+
+    printf("\nThere are %d safe reports out of %d\n", safeReports, it);
 
     fclose(reportstext);
 
